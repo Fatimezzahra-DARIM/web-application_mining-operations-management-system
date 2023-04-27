7d64772edbb0003e708d8d68ccc7a986cb93ebd0
Fatimezzahradarim <fatimezzahradarim@gmail.com>
2023-04-27 11:14:19 +0100

    add modal for add task

--- resources/views/template/geologistDashboard/file.blade.php
+++ resources/views/template/geologistDashboard/file.blade.php
@@ -6,6 +6,58 @@
 <h1 class="flex items-center font-sans font-bold break-normal text-indigo-500 px-2 py-8 text-xl md:text-2xl">
     File Task
 </h1>
+<!-- Main modal -->
+<div id="authentication-modal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative w-full max-w-md max-h-full">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="authentication-modal">
+                <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                <span class="sr-only">Close modal</span>
+            </button>
+            <div class="px-6 py-6 lg:px-8">
+                <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white"> Add File Task</h3>
+                <form class="space-y-6" action="{{ route('tasks.store') }}" method="POST">
+                          @csrf
+                          <div class="mb-4">
+                            <label class="block text-gray-700 font-bold mb-2" for="description">
+                              Description
+                            </label>
+                            <textarea
+                              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                              id="description"
+                              name="description"
+                              placeholder="Enter a description..."
+                              rows="4"
+                            ></textarea>
+                          </div>
+                          <div class="mb-4">
+                            <label class="block text-gray-700 font-bold mb-2" for="pdf">
+                              Upload PDF
+                            </label>
+                            <input
+                              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                              id="pdf"
+                              name="pdf"
+                              type="file"
+                            />
+                          </div>
+                        
+                        {{-- <button type="submit">okgdfg</button> --}}
+                        <div class="flex justify-center">
+                        <button
+                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="submit"
+                      >
+                        Submit
+                      </button>
+                      </div>
+
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 <!--Card-->
